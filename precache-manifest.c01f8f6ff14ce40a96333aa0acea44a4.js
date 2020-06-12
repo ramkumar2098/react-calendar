72c3df55309b3ce6ea331780540181fdfe99b526
ramkumar2098 <ramkumar2098@protonmail.com>
2020-06-12 19:51:53 +0530

    Updates

---
+++ precache-manifest.c01f8f6ff14ce40a96333aa0acea44a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e166f5785edd94efb012d237b6d9ea7",
+    "url": "/react-calendar/index.html"
+  },
+  {
+    "revision": "aec45737f81252b10de9",
+    "url": "/react-calendar/static/css/main.5ca9c136.chunk.css"
+  },
+  {
+    "revision": "196fa7f9b5d60edce5b6",
+    "url": "/react-calendar/static/js/2.20908ee0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calendar/static/js/2.20908ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec45737f81252b10de9",
+    "url": "/react-calendar/static/js/main.151f10e4.chunk.js"
+  },
+  {
+    "revision": "b85558a34156120929ad",
+    "url": "/react-calendar/static/js/runtime-main.fe94308e.js"
+  }
+]);
